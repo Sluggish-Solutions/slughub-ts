541f74f9e5c120229c3a016ca3d6fc80790d94aa
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-01-13 11:30:17 -0800

    😀

---
+++ src/lib/components/home/Post.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+
+	// need to add user types later
+	export let info: any;
+</script>
+
+<div class="container">
+	<!-- top segment: avatar and name -->
+	<section class="flex">
+		<Avatar src={info.user_profile_img} />
+		<h3>
+			{info.user_name}
+		</h3>
+
+	</section>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,26 @@
+<script>
+	import Post from '$lib/components/home/Post.svelte';
+	import { Send } from 'lucide-svelte'
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+	// on load, home page will get an array of posts (objects); data should get the first 3 posts, each containing (post object and user object)
+	export let data
+
+	let post = {
+		info: {
+			id: 1,
+			title: 'test post',
+			content: 'test content',
+			likes: 0,
+			comments: 0,
+			created_at: '2021-10-10 10:10:10'
+		},
+		user: {
+			id: 1,
+			name: 'john',
+			profile_img: 'https://i.imgur.com/8Km9tLL.png'
+		}
+	}
+</script>
+
+<!-- <Post info={test_user} /> -->
+<Send />
