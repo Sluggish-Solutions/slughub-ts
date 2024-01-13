3aa220551d9899aaf4c0a0fe9d91d4b5da9d1297
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-01-13 12:17:14 -0800

    😀

--- src/lib/components/home/Post.svelte
+++ src/lib/components/home/Post.svelte
@@ -1,17 +1,43 @@
 <script lang="ts">
 	import { Avatar } from '@skeletonlabs/skeleton';
+	import { Heart, MessageCircle, Send, Bookmark } from 'lucide-svelte';
 
 	// need to add user types later
 	export let info: any;
 </script>
 
-<div class="container">
+<div class="w-1/2 m-auto">
 	<!-- top segment: avatar and name -->
-	<section class="flex">
-		<Avatar src={info.user_profile_img} />
+	<section class="flex items-center">
+		<Avatar src={info.user.profile_img} />
 		<h3>
-			{info.user_name}
+			{info.user.name}
 		</h3>
+	</section>
 
+	<!-- image post -->
+	<section>
+		<img src={info.details.img} alt={info.details.description} />
+	</section>
+
+	<!-- interaction bar: like, comment, share, bookmark -->
+	<section>
+		<div class="flex justify-between items-center h-11 p-1">
+			<div class="left flex">
+				<!-- lucide icons, can change color, size, strokeWidth -->
+				<div class="p-1">
+					<Heart />
+				</div>
+				<div class="p-1">
+					<MessageCircle />
+				</div>
+				<div class="p-1">
+					<Send />
+				</div>
+			</div>
+			<div class="right p-1">
+				<Bookmark />
+			</div>
+		</div>
 	</section>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,19 +5,19 @@
 	export let data
 
 	let post1 = {
-		info: {
+		details: {
 			id: 1, // post_id
 			user_id: 1,
+			img: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 			description: 'this is the first post',
-			content: 'test content',
 			likes: 0,
-			comments: 0,
+			comments: ["that's great", "wow", "..."],
 			created_at: '2021-10-10 10:10:10'
 		},
 		user: {
 			id: 1,
 			name: 'john',
-			profile_img: 'https://i.imgur.com/8Km9tLL.png'
+			profile_img: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 		}
 	}
 </script>
