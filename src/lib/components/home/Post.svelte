e3aa2a02de637e4f98881c38f1d970adf54570a9
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-01-13 15:49:49 -0800

    😀

--- src/lib/components/home/Post.svelte
+++ src/lib/components/home/Post.svelte
@@ -4,6 +4,17 @@
 
 	// need to add user types later
 	export let info: any;
+
+	let showMore = false;
+	let showComments = false;
+
+	const toggleReadMore = () => {
+		showMore = !showMore;
+	};
+
+	const toggleComments = () => {
+		showComments = !showComments;
+	};
 </script>
 
 <div class="m-auto">
@@ -17,7 +28,7 @@
 
 	<!-- image post -->
 	<section>
-		<img src={info.details.img} alt={info.details.description} />
+		<img src={info.details.img} alt={info.details.description} width={500} />
 	</section>
 
 	<!-- interaction bar: like, comment, share, bookmark -->
@@ -40,4 +51,49 @@
 			</div>
 		</div>
 	</section>
+
+	<!-- likes & description -->
+	<section class="font-light">
+		<strong class="font-bold block">
+			{info.details.likes} Likes
+		</strong>
+		<strong class="font-bold">{info.user.name}</strong>
+
+		{#if !showMore}
+			{info.details.description.substring(0, 50)} ...<button
+				on:click={toggleReadMore}
+				class="btn p-0 text-slate-300">more</button
+			>
+		{:else}
+			{info.details.description}
+		{/if}
+
+		{#if !showComments}
+			<button on:click={toggleComments} class="btn p-0 text-slate-300">
+				View all {info.details.comments.length} comments
+			</button>
+		{:else}
+			{#each info.details.comments as comment}
+				<p>
+					{comment}
+				</p>
+			{/each}
+		{/if}
+	</section>
 </div>
+
+<!-- <div class="post">
+	{#if showMore}
+		<div class="more-content">
+			<p>Additional text goes here...</p>
+		</div>
+	{/if}
+</div>
+
+<button on:click={toggleReadMore}>
+	{#if showMore}
+		Read less
+	{/if}
+	{!showMore} Read more
+</button>
+<div /> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,49 @@
-<script lang='ts'>
+<script lang="ts">
 	import Post from '$lib/components/home/Post.svelte';
 
 	// on load, home page will get an array of posts (objects); data should get the first 3 posts, each containing (post object and user object)
-	export let data
+	export let data;
 
 	let post1 = {
 		details: {
 			id: 1, // post_id
 			user_id: 1,
 			img: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-			description: 'this is the first post',
-			likes: 0,
-			comments: ["that's great", "wow", "..."],
+			description: 'This is the first post that you will like for sure, I bet you!',
+			likes: 1023,
+			comments: [
+				{
+					id: 1,
+					text: "that's great",
+					user: {
+						id: 1,
+						name: 'john',
+						profile_img:
+							'https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+					},
+					createdAt: 23234
+				},
+				{
+					id: 2,
+					text: 'wow',
+					user: {
+						id: 3,
+						name: 'kane',
+						profile_img:
+							'https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+					},
+					createdAt: 73854
+				},
+			],
 			created_at: '2021-10-10 10:10:10'
 		},
 		user: {
 			id: 1,
 			name: 'john',
-			profile_img: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+			profile_img:
+				'https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 		}
-	}
+	};
 </script>
 
 <Post info={post1} />
-
