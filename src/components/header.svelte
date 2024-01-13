45831643fb27c8d265ae1a456c99b1d400994363
Alb111 <albertfelix2005@gmail.com>
2024-01-13 11:31:01 -0800

    top navbar inprogress

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,1 +1,17 @@
-<>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Raleway:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body class="font-comfortaa">
+
+    <nav class="flex justify-between items-center bg-black px-2">
+        <div class="flex items-center">
+            <img class="max-h-8 " src="/icon.png" alt="Icon">
+            <h1 class="m-4 font-raleway">SlugHub</h1>
+        </div>
+        <!-- Img below created by Ivan Abirawa - Flaticon -->
+        <img class="max-h-8 mr-2" src="/email.png" alt="Email Icon">
+    </nav>
+
+</body>
