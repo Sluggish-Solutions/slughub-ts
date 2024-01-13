543f45a95997ca060935014021e6400d981e60a8
Surendra <suri312006@gmail.com>
2024-01-12 22:03:18 -0800

    adding types

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,16 @@
 
 // src/hooks.server.ts
-// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_API_KEY } from '$env/static/public'
-// import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
-// import type { Database } from '../types/supabase.types.js'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
+import type { Database } from '../types/supabase.types.js'
 import type { Handle } from '@sveltejs/kit'
-
+// hooks.server.ts basically runs the code inside whenever 
 export const handle: Handle = async ({ event, resolve }) => {
-  // event.locals.supabase = createSupabaseServerClient<Database>({
-  //   supabaseUrl: PUBLIC_SUPABASE_URL,
-  //   supabaseKey: PUBLIC_SUPABASE_API_KEY,
-  //   event,
-  // })
+  event.locals.supabase = createSupabaseServerClient<Database>({
+    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseKey: PUBLIC_SUPABASE_API_KEY,
+    event,
+  })
 
   /**
    * A convenience helper so we can just call await getSession() instead const { data: { session } } = await supabase.auth.getSession()
@@ -23,6 +23,7 @@ export const handle: Handle = async ({ event, resolve }) => {
   // }
 	console.log(event);
   return resolve(event, {
+		
     filterSerializedResponseHeaders(name) {
       return name === 'content-range'
     },
