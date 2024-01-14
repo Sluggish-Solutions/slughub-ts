1022a0715377d431fe706be1b3e6669d3420f45f
Alb111 <albertfelix2005@gmail.com>
2024-01-13 16:14:08 -0800

    transitions

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,7 +6,7 @@
 <nav class="flex justify-between items-center bg-grey px-2 sticky top-0">
     <div class="flex items-center">
         <img class="max-h-10 " src="/icon.png" alt="Icon">
-        <h1 class="m-3 font-raleway text-2xl">SlugHub</h1>
+        <h1 class="font-raleway text-2xl">SlugHub</h1>
     </div>
     <div class="=max-h-8 mr-2">
         <Send />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,12 @@
     import SideNav from '$lib/components/SideNav.svelte';
 </script>
 
-<div class="flex">
-	<div class="sticky top-0 z-20 py-20 px-5 bg-black">
+<div class="flex justify-between">
+	<div class="sticky top-0 z-20 py-3 px-10 bg-black hidden md:block">
 		<SideNav/>
 	</div>
 	<div>
-		<div class="sticky top-0 z-20 p-4 bg-black">
+		<div class="sticky top-0 z-20 p-4 bg-black sm:hidden">
 			<Header />
 		</div>
 
