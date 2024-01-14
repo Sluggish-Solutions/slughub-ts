16b1973d04e94b5ec60783ab5f106f5586d66c48
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-01-14 13:56:23 -0800

    loaded data from supabase

--- src/lib/components/home/Comment.svelte
+++ src/lib/components/home/Comment.svelte
@@ -7,12 +7,12 @@
 <div class="flex">
 	<section class="p-2">
 		<Avatar
-			src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop"
+			src={comment.author_img_url}
 			width="w-12"
 			rounded="rounded-full"
 		/>
 	</section>
 	<section class="p-1 pt-2">
-		<p class="font-light"><strong class="font-bold text-sm">{comment.user.name}</strong> {comment.text}</p>
+		<p class="font-light"><strong class="font-bold text-sm">{comment.author}</strong> {comment.body}</p>
 	</section>
 </div>
