447f699ba958d3e0cab8f55a7d665bb55980997b
Surendra <suri312006@gmail.com>
2024-01-15 11:38:50 -0800

    added get all user data inside of userStore

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -10,6 +10,15 @@
         postArray: ["https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ],
         profileImage: "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     };
+
+    import {getAllUserData} from '$stores/userStore';
+    import {onMount} from 'svelte'
+    onMount(async ()=>{
+    let user_data = await getAllUserData()
+    console.log("object user data, ", user_data.data)
+
+    })
+
 </script>
 
 <Profile userData={John}/>
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -5,3 +5,23 @@ export const curr_user_id = writable('');
 export const setUserId = (id: any) => {
     curr_user_id.set(id);
 }
+
+
+export const getAllUserData  = async () => {
+	let user_id = ''
+	curr_user_id.subscribe((value) => user_id = value);
+
+	const res = await fetch('/api/getAllUserData', {
+		method: 'POST',
+		body: JSON.stringify({
+			"user_id": user_id,
+		})
+	})
+	const response = await res.json()
+	console.log(response)
+return response	
+	console.log("post store response", response);
+// ideally it would be nice if there was some sort of toast if some error happend?
+	//
+	
+}
