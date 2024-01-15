67302d7b9893832cb2b74132bea84cd075ba9625
Surendra <suri312006@gmail.com>
2024-01-14 19:27:59 -0800

    lets see if this works

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -3,5 +3,6 @@ import { writable } from 'svelte/store';
 export const curr_user_id = writable('');
 
 export const setUserId = (id: any) => {
+	console.log('setting user id', id)
     curr_user_id.set(id);
 }
