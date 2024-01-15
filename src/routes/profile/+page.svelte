d0c0d4fd578117aaeb1938fc6a0dec57d641cb4d
Alb111 <albertfelix2005@gmail.com>
2024-01-15 14:32:34 -0800

    yo plz work

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import Profile from '$lib/components/Profile.svelte';
-    import Post from "$lib/components/home/Post.svelte";
-    import {getAllUserData} from "$stores/userStore";
-    let x:any = getAllUserData();
-    console.log(x);
 </script>
+
 <h1>dafdsf</h1>
 
-<!-- <Profile {}/> -->
+<Profile/>
