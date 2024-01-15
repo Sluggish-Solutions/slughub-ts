c94d171c17adcbde32afb5db83c9aa37107bb923
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-01-15 14:04:40 -0800

    upload images working

--- src/lib/components/modals/CreatePost.svelte
+++ src/lib/components/modals/CreatePost.svelte
@@ -2,12 +2,12 @@
 	import { getModalStore } from '@skeletonlabs/skeleton';
 	import { FileDropzone, FileButton } from '@skeletonlabs/skeleton';
 	import { FileUp } from 'lucide-svelte';
-	import {createPost} from '$stores/postStore'
+	import { createPost } from '$stores/postStore';
 	const modalStore = getModalStore();
 
 	let files: FileList;
-	let uploaded = false;
 	$: base64Image = '';
+	let descValue = '';
 
 	const handleFileChange = (e: Event) => {
 		if (e.target) {
@@ -22,27 +22,34 @@
 			};
 
 			reader.readAsDataURL(selectedImage);
-
-
 		}
 	};
 </script>
 
 {#if $modalStore[0]}
 	<main class="w-[100vw] h-[100vh] flex justify-center items-center backdrop-blur-md">
-		{#if files && files[0]}
-			<!-- svelte-ignore a11y-img-redundant-alt -->
-			<div class="flex flex-col gap-3 p-3">
-				<img
-					src={base64Image}
-					alt="Uploaded image"
-					class="max-w-[300px] max-h-[500px] md:max-w-[500px] md:max-h-[700px]"
-				/>
+		<form on:submit={submitPost}>
+			{#if files && files[0]}
+				<!-- svelte-ignore a11y-img-redundant-alt -->
+				<div class="flex flex-col gap-3 p-3">
+					<img
+						src={base64Image}
+						alt="Uploaded image"
+						class="max-w-[300px] max-h-[500px] md:max-w-[500px] md:max-h-[500px]"
+					/>
 
-				<FileButton name="files" on:change={handleFileChange}>Upload new Image</FileButton>
-			</div>
-		{:else}
-			<FileButton name="files" on:change={handleFileChange}>Upload an Image</FileButton>
-		{/if}
+					<FileButton name="files" on:change={handleFileChange}>Upload new Image</FileButton>
+				</div>
+
+				<label class="label">
+					<span>Description</span>
+					<input class="input" type="text" placeholder="Input" bind:value={descValue} />
+				</label>
+
+				<button type="submit" class="btn variant-filled-secondary my-3">Post</button>
+			{:else}
+				<FileButton name="files" on:change={handleFileChange}>Upload an Image</FileButton>
+			{/if}
+		</form>
 	</main>
 {/if}
