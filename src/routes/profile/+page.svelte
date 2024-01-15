bd0c6b4182f8de88aa3680c8fb2ebbebb3fd7692
Surendra <suri312006@gmail.com>
2024-01-15 11:45:44 -0800

    fixed user posts getting lixicolog

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -10,11 +10,11 @@
         postArray: ["https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ],
         profileImage: "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     };
-
-    import {getAllUserData} from '$stores/userStore';
+//@ts-expect-error i dont why why it says that getAllUserData is not exported cuz it literally is
+    import {getCurrUserData} from '$stores/userStore';
     import {onMount} from 'svelte'
     onMount(async ()=>{
-    let user_data = await getAllUserData()
+    let user_data = await getCurrUserData()
     console.log("object user data, ", user_data.data)
 
     })
